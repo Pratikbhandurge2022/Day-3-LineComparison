7ead85e84060b27c82c507f6c61185f1d4ac2b0d
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-13 18:53:38 +0530

    uc1 & uc2

---
+++ line comp/line comp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace line_comp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!, Welcome to LineComparison Computaion");
+
+            Console.WriteLine("=========UC1==========");
+            UC1 u1 = new UC1();
+            u1.Length();
+
+            Console.WriteLine("=========UC2==========");
+            UC2 u2 = new UC2();
+            u2.EqualityOfLines();
+        }
+    }
+}
